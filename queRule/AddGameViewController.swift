51c7207dc116a864d1ce15262e62fd95f18a3eec
Benjamín Garrido Barreiro <benjamin.garrido@gmail.com>
2017-01-08 14:44:31 +0100

    coding viewDidLoad function logic

--- queRule/AddGameViewController.swift
+++ queRule/AddGameViewController.swift
@@ -34,7 +34,58 @@ class AddGameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        dateFormatter.dateFormat = "dd/MM/yyyy"
         
+        /* Keyboard */
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+        self.view.addGestureRecognizer(tapGesture)
+        
+        let takePictureGesture = UITapGestureRecognizer(target: self, action: #selector(takePictureTapped))
+        self.gameImageView.addGestureRecognizer(takePictureGesture)
+        
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        datePicker = UIDatePicker(frame: CGRect(x: 0, y: 210, width: 320, height: 216))
+        datePicker.datePickerMode = .date
+        datePicker.addTarget(self, action: #selector(datePickerChangedValue), for: .valueChanged)
+        txtBorrowedDate.inputView = datePicker
+        
+        if game == nil {
+            // Estamos creando un nuevo juego
+            self.title = "Añadir juego"
+            self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonPressed))
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveButtonPressed))
+            
+            self.btnDelete.isHidden = true
+            borrrowedSwitch.isOn = false
+        } else {
+            // Estamos modificando un juego
+            self.title = "Detalles"
+            self.txtTitle.text = game?.title
+            if let borrowed = game?.borrowed {
+                self.borrrowedSwitch.isOn = borrowed
+            }
+            
+            self.txtBorrowedTo.text = game?.borrowedTo
+            if let borrowedDate = game?.borrowedDate as? Date {
+                self.txtBorrowedDate.text = dateFormatter.string(from: borrowedDate)
+            }
+            
+            if let imageData = game?.image as? Data {
+                self.gameImageView.image = UIImage(data: imageData)
+            }
+            
+            self.btnDelete.isHidden = false
+        }
+        
+        if !borrrowedSwitch.isOn {
+            txtBorrowedTo.isEnabled = false
+            txtBorrowedDate.isEnabled = false
+        }
     }
 
 }
